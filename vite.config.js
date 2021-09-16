07fb7dc42943ab24630a05a9bd8a03c424bdeea1
Joshua Oertel <joshwashywash@gmail.com>
2021-09-15 20:39:16 -0700

    update sw generator

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,34 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { injectManifest } from "rollup-plugin-workbox";
+import { generateSW } from "rollup-plugin-workbox";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/searchmons/",
   plugins: [
     svelte(),
-    injectManifest({
-      mode: "production",
-      swDest: "dist/sw.js",
-      swSrc: "sw.js",
+    generateSW({
       globDirectory: "public/",
       globPatterns: ["**/*.{ico,png,json,txt}"],
+      runtimeCaching: [
+        {
+          // Match any request that ends with .png, .jpg, .jpeg or .svg.
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+          // Apply a cache-first strategy.
+          handler: "CacheFirst",
+
+          options: {
+            // Use a custom cache name.
+            cacheName: "images",
+
+            expiration: {
+              maxEntries: 30,
+            },
+          },
+        },
+      ],
+      sourcemap: false,
+      swDest: "dist/sw.js",
     }),
   ],
 });
