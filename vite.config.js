6415100b67de08fc6eeddd49707d94b3f18a1522
Joshua Oertel <joshwashywash@gmail.com>
2021-09-18 11:17:35 -0700

    testing manifest start_url

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,27 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { generateSW } from "rollup-plugin-workbox";
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { generateSW } from 'rollup-plugin-workbox';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/searchmons/",
+  base: '/searchmons/',
   plugins: [
     svelte(),
     generateSW({
-      globDirectory: "public/",
-      globPatterns: ["**/*.{ico,png,json,txt}"],
+      clientsClaim: true,
+      globDirectory: 'public/',
+      globPatterns: ['**/*.{ico,png,json,txt}'],
       runtimeCaching: [
         {
           // Match any request that ends with .png, .jpg, .jpeg or .svg.
           urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
 
           // Apply a cache-first strategy.
-          handler: "CacheFirst",
+          handler: 'CacheFirst',
 
           options: {
             // Use a custom cache name.
-            cacheName: "images",
+            cacheName: 'images',
 
             expiration: {
               maxEntries: 30,
@@ -29,7 +30,7 @@ export default defineConfig({
         },
       ],
       sourcemap: false,
-      swDest: "dist/sw.js",
+      swDest: 'dist/sw.js',
     }),
   ],
 });
