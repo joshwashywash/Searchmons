269101fc7f979b97b468c3abff207a11351fa88e
Joshua Oertel <joshwashywash@gmail.com>
2021-09-18 17:54:58 -0700

    i'm not gonna mess with it anymore

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,18 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { generateSW } from 'rollup-plugin-workbox';
+import { injectManifest } from 'rollup-plugin-workbox';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/searchmons/',
-  plugins: [
-    svelte(),
-    generateSW({
-      clientsClaim: true,
-      globDirectory: 'public/',
-      globPatterns: ['**/*.{ico,png,json,txt}'],
-      runtimeCaching: [
-        {
-          // Match any request that ends with .png, .jpg, .jpeg or .svg.
-          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-
-          // Apply a cache-first strategy.
-          handler: 'CacheFirst',
-
-          options: {
-            // Use a custom cache name.
-            cacheName: 'images',
-
-            expiration: {
-              maxEntries: 30,
-            },
-          },
-        },
-      ],
-      sourcemap: false,
-      swDest: 'dist/sw.js',
-    }),
-  ],
+	base: '/searchmons/',
+	plugins: [
+		injectManifest({
+			globDirectory: 'public/',
+			globPatterns: ['**/*.{ico,png,json,txt}'],
+			mode: 'production',
+			swDest: 'dist/sw.js',
+			swSrc: 'public/sw.js'
+		}),
+		svelte()
+	]
 });
