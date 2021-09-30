ae2a01440c9fde23888c7240e33ba4732695db93
Joshua Oertel <joshwashywash@gmail.com>
2021-09-29 21:01:09 -0700

    refactor: make eventlistener on body conditional

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -6,9 +6,7 @@ const dev = process.env.NODE_ENV === 'development';
 
 module.exports = {
 	plugins: [
-		//Some plugins, like postcss-nested, need to run before Tailwind,
 		tailwindcss(),
-		//But others, like autoprefixer, need to run after,
 		autoprefixer(),
 		!dev &&
 			cssnano({
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,6 +2,11 @@ import type { Pokemon } from '../types';
 import { writable } from 'svelte/store';
 import { createToggler } from './toggler';
 
+import resolveConfig from 'tailwindcss/resolveConfig';
+
+const config = resolveConfig(false);
+console.log(config.theme.screens.md);
+
 export const open = createToggler(false);
 
 export const selectedPokemon = writable<Pokemon>(null);
