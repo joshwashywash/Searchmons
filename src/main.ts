5e9db178f34cf1e898da306ca56c1ae2fddeec4a
Joshua Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-05-01 18:21:42 -0700

    remove unnecessary import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import App from './App.svelte';
 import { fetchJSON } from './functions';
 import type { Pokemon, PokemonResult } from './types';
-import { selectedPokemon } from './stores';
 
 const json: { results: PokemonResult[] } = await fetchJSON(
   'https://pokeapi.co/api/v2/pokemon?limit=151'
