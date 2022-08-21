a17dc06bbd758547c82b70e8276dc3c737001082
Joshua Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-08-20 18:00:57 -0700

    testing deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/searchmons/',
   plugins: [svelte()],
   build: {
     minify: 'esbuild',
